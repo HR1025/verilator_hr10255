95c092e0fc77d84f6f692abdedc8a38e94edf289
王浩锐 <1905811497@qq.com>
2022-01-03 14:03:15 +0800

    [what] 添加有关于网表定义的相关信息
    [why] 完整地描述层次化网表
    [how]
    1. 修改 MoudleMsg ，添加新的成员变量
    2. 添加 in ,out, wire, inout 以及 assign 的数据结构支持

--- src/netlistsdefine.cpp
+++ src/netlistsdefine.cpp
@@ -18,3 +18,23 @@ std::vector<int> PortInstanceFormalMsg::getIndexs()
 
     return res;
 }
+
+uint32_t MoudleMsg::inNum(bool onlyIn) const
+{
+    return onlyIn ? inputs.size() : inputs.size() + inouts.size();
+}
+
+uint32_t MoudleMsg::outNum(bool onlyOut) const
+{
+    return onlyOut ? outputs.size() : outputs.size() + inouts.size();
+}
+
+uint32_t MoudleMsg::inoutNum() const
+{
+    return inouts.size();
+}
+
+uint32_t MoudleMsg::wireNum() const
+{
+    return wires.size();
+}
